b1f847918d037b22f8c9780c0462a5169abcbdaa
mech071 <suprit2snehasis@gmail.com>
2026-04-14 18:54:05 +0530

    oops

--- app/user/signup/page.js
+++ app/user/signup/page.js
@@ -7,16 +7,17 @@ import { useSession } from "next-auth/react"
 import { useRouter } from "next/navigation"
 import { useEffect } from "react"
 
-const { data: session } = useSession()
-const router = useRouter()
-
-useEffect(() => {
-  if (session) {
-    router.push("/")
-  }
-}, [session])
 
 const Signup = () => {
+  const { data: session } = useSession()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (session) {
+      router.push("/")
+    }
+  }, [session])
+
   return (
     <>
       <main className="min-h-screen bg-white flex items-center justify-center px-6">
